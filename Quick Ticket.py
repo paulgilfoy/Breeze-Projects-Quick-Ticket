c908bfce4d0ffc55e31e45a40f7819b7b940136b
Paul Gilfoy <paul.gilfoy+github@gmail.com>
2021-07-22 23:50:23 -0400

    Initial upload of Breeze "quick ticket" script

---
+++ Quick Ticket.py
@@ -0,0 +1,37 @@
+import time, requests
+
+mylocaltime = time.strftime('%Y-%m-%d', time.localtime())
+
+
+print('Hello')
+
+print('Task Name?')
+print('Department : System : Problem')
+print('ex. CS : Acumatica : User Permissions issue')
+breezename = input()
+
+print('Description?')
+breezedescription = input()
+
+url = "http://api.breeze.pm/projects/124084/cards.json?api_token=NXzKkVGTBWkCAUB4KznA"
+
+payload ={"name": breezename,
+    "description": breezedescription,
+    "startdate": mylocaltime,
+    "planned_time": 120,
+    "color": "c25bg",
+    "stage_id": 576497,
+    "swimlane_id": '',
+    "invitees": ["paulg@bungalow5.com"],
+    "user": {
+        "id": 56539,
+        "email": "paulg@bungalow5.com",
+        "name": "Paul Gilfoy"}}
+headers = {
+  'Content-Type': 'application/json'
+}
+
+response = requests.post(url, json=payload)
+
+print(response.text.encode('utf8'))
+
